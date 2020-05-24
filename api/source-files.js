e095387c8b0e1dcbe0a1d6539f3670f912160696
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-24 11:43:39 +0000

    Deploy chinedufn/landon to github.com/chinedufn/landon.git:gh-pages

---
+++ api/source-files.js
@@ -0,0 +1,4 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["blender_armature"] = {"name":"","files":["convert.rs","coordinate_system.rs","export.rs","interpolate.rs","lib.rs"]};
+sourcesIndex["blender_mesh"] = {"name":"","dirs":[{"name":"combine_indices","files":["create_single_index_config.rs","mod.rs","weighted_normals.rs"]},{"name":"vertex_attributes","dirs":[{"name":"single_indexed","files":["interleave.rs"]}],"files":["mod.rs","single_indexed.rs","vertex_attribute.rs"]}],"files":["bone.rs","bounding_box.rs","export.rs","face_tangents.rs","interleave.rs","lib.rs","material.rs","triangulate.rs","y_up.rs"]};
+createSourceSidebar();
